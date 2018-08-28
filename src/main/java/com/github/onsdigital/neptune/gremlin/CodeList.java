81562debb078070afe022978bf0b9158200e09ce
Dave Llewellyn <439837@gmail.com>
2018-08-28 14:53:39 +0100

    fix bug

--- src/main/java/com/github/onsdigital/neptune/gremlin/CodeList.java
+++ src/main/java/com/github/onsdigital/neptune/gremlin/CodeList.java
@@ -1,6 +1,9 @@
 package com.github.onsdigital.neptune.gremlin;
 
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.apache.tinkerpop.gremlin.structure.T;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 
 import static com.github.onsdigital.neptune.logging.LogBuilder.logBuilder;
 
@@ -8,13 +11,8 @@ public class CodeList implements GremlinTask{
 
     @Override
     public void execute(GraphTraversalSource g) {
-        Object ID = g.addV("_code_list")
-                .property("name", "gibson-guitars")
-                .property("edition", "2018")
-                .property("label", "fender")
-                .next()
-                .id();
-
-        logBuilder().info("CODE LIST ID " + ID.toString());
+        Graph graph = g.getGraph();
+        Vertex v1 = graph.addVertex(T.label, "_code_list", "name", "gibson-guitars", "edition", "2018", "label", "fender");
+        logBuilder().info("created vertex ID: " + v1.id());
     }
 }
